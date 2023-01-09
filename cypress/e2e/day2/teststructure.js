ccf2fa216885caa631707308eafcc560f500650c
Blgymsk <bilgeymsk@gmail.com>
2023-01-09 16:58:30 +0100

    first commit

---
+++ cypress/e2e/day2/teststructure.js
@@ -0,0 +1,39 @@
+
+
+/*describe('First Test Suit ',()=>{  //Test Suitlerimiz function yerine bu sekilde de yazilabilir!!
+
+    it(' First Test Case',()=>{  //Test Caselerimizi bu sekilde yaziyoruz 
+
+        syntax 
+    })
+    it(' Second Test Case',()=>{
+
+        syntax 
+    })
+    it(' Third Test Case',()=>{
+
+        syntax 
+    })
+})
+*/
+
+
+describe('First Test Suit ',function(){  //Test Suitlerimiz
+
+    beforeEach(()=>{   //bu sekilde her test case den önce bu calisacak
+        cy.visit('')
+    })
+
+    it(' First Test Case',function(){  //Test Caselerimizi bu sekilde yaziyoruz 
+
+        syntax 
+    })
+    it(' Second Test Case',function(){
+
+        syntax 
+    })
+    it(' Third Test Case',function(){
+
+        syntax 
+    })
+})
