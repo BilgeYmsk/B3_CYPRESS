b8e88a48606ccb09adb55dac8b22325681071a73
Blgymsk <bilgeymsk@gmail.com>
2023-01-11 15:27:13 +0100

    Cypress_Framework

--- cypress/e2e/Test/firstTest.cy.js
+++ cypress/e2e/Test/firstTest.cy.js
@@ -6,10 +6,10 @@ describe('First Test ', () => {
     it('TC001 Google Test', () => {
 
         cy.visit('https://www.google.de/');
-        cy.get('#L2AGLb > .QS5gu').click();
+        cy.get('#L2AGLb > .QS5gu').click()
         cy.get('.gLFyf').should('be.visible').type('cypress{enter}');
-        cy.url().should('include','cypress');
-        cy.get('[href="https://www.cypress.io/"] > .LC20lb')
+        cy.url().should('include','cypress')
+        cy.get('[href="https://www.cypress.io/"] > .TbwUpd > .iUh30')
           .should('have.text','JavaScript End to End Testing Framework | cypress.io testing ...')
           .click()
 
