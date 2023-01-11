e69848b873457a3cdc0d7f4a846288c88c739980
Blgymsk <bilgeymsk@gmail.com>
2023-01-11 15:55:08 +0100

    Test_üben

--- cypress/e2e/Test/firstTest.cy.js
+++ cypress/e2e/Test/firstTest.cy.js
@@ -9,7 +9,7 @@ describe('First Test ', () => {
         cy.get('#L2AGLb > .QS5gu').click()
         cy.get('.gLFyf').should('be.visible').type('cypress{enter}');
         cy.url().should('include','cypress')
-        cy.get('[href="https://www.cypress.io/"] > .TbwUpd > .iUh30')
+        cy.get('[href="https://www.cypress.io/"] > .LC20lb')
           .should('have.text','JavaScript End to End Testing Framework | cypress.io testing ...')
           .click()
 
